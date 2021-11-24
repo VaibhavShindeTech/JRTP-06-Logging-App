8c0e20dc97a551b75990545744ea2eb1e484fedc
Vaibhav Shinde <vaibhavshinde1642@gmail.com>
2021-11-24 10:39:41 +0530

    created logging app for implement slf4

---
+++ src/main/java/com/ashokit/Jrtp06LoggingAppApplication.java
@@ -0,0 +1,13 @@
+package com.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jrtp06LoggingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jrtp06LoggingAppApplication.class, args);
+	}
+
+}
